7034a35a4038f01ec76408a0b64c8ccdfde79a23
Josh T <joshliam09@gmail.com>
2021-06-12 15:27:22 -0700

    chore: wrong index was being used in the losing h2h condition

--- main.go
+++ main.go
@@ -192,7 +192,7 @@ func detectHeadToHead(us *Coord, board *Board, ourLength int32, validMoves []str
 		for i := 0; i < len(movesUs); i++ {
 			futureUs := movesUs[i]
 			for j := 0; j < len(movesEnemy); j++ {
-				futureEnemy := movesEnemy[i]
+				futureEnemy := movesEnemy[j]
 				if (futureUs.X != futureEnemy.X || futureUs.Y != futureEnemy.Y) && isMovePossible(us, board, indexToMove(i)) {
 					return indexToMove(i)
 				}
