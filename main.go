d24d83229f6cbdd063dad7b513e4a0056471d99c
Josh T <joshliam09@gmail.com>
2021-06-17 17:50:49 -0700

    feat: snake prioritises getting out of hazard over food again

--- main.go
+++ main.go
@@ -61,26 +61,7 @@ func HandleMove(w http.ResponseWriter, r *http.Request) {
 	// if there is a potential head to head, go for it if we can win, else avoid
 	move := "null"
 	move = snake.DetectHeadToHead(&head, &request.Board, request.You.Length)
-	fmt.Printf("brraaaaarr\n")
-	// else, find closest food and path to it if possible
-	if move == "null" {
-		// TODO: put this in a function in snake.go
-		dist := 90000 // TODO: change this to actual max value of int, lookup golang spec
-		food := request.Board.Food
-		var closestFood datatypes.Coord
-		for i := 0; i < len(food); i++ {
-			if (food[i].X-head.X)*(food[i].X-head.X)+(food[i].Y-head.Y)*(food[i].Y-head.Y) < dist {
-				dist = (food[i].X-head.X)*(food[i].X-head.X) + (food[i].Y-head.Y)*(food[i].Y-head.Y)
-				closestFood = food[i]
-			}
-		}
 
-		// attempt to go in the direction of the closestFood
-		fmt.Printf("hehe\n")
-		move = snake.MoveInDirection(&head, &closestFood, &request.Board)
-		fmt.Printf("yay\n")
-	}
-	fmt.Printf("aaaa\n")
 	// else, if we are in hazard and health is <=50, find the closest not-hazard square and move towards it if possible
 	if move == "null" {
 		// check if head is in a hazard and health is <= half
@@ -107,7 +88,24 @@ func HandleMove(w http.ResponseWriter, r *http.Request) {
 			move = snake.MoveInDirection(&head, &safeCoord, &request.Board)
 		} // end if "in hazard and health low"
 	} // end if move == null
-	fmt.Printf("brrrr\n")
+
+	// else, find closest food and path to it if possible
+	if move == "null" {
+		// TODO: put this in a function in snake.go
+		dist := 90000 // TODO: change this to actual max value of int, lookup golang spec
+		food := request.Board.Food
+		var closestFood datatypes.Coord
+		for i := 0; i < len(food); i++ {
+			if (food[i].X-head.X)*(food[i].X-head.X)+(food[i].Y-head.Y)*(food[i].Y-head.Y) < dist {
+				dist = (food[i].X-head.X)*(food[i].X-head.X) + (food[i].Y-head.Y)*(food[i].Y-head.Y)
+				closestFood = food[i]
+			}
+		}
+
+		// attempt to go in the direction of the closestFood
+		move = snake.MoveInDirection(&head, &closestFood, &request.Board)
+	}
+
 	// if all other cases don't apply just pick a random move
 	var legalMoves []string
 	if snake.IsMovePossible(&head, &request.Board, "up") {
