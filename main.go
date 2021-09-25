8fda5b08cde002fd9d7941b082efe458b883deea
Josh T <joshliam09@gmail.com>
2021-09-24 21:50:03 -0700

    feat: moved some stuff and started minimax

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 
 // HandleStart is called at the start of each game your Battlesnake is playing.
 // The GameRequest object contains information about the game that's about to start.
-// TODO: Use this function to decide how your Battlesnake is going to look on the board.
 func HandleStart(w http.ResponseWriter, r *http.Request) {
 	request := datatypes.GameRequest{}
 	err := json.NewDecoder(r.Body).Decode(&request)
@@ -47,57 +46,7 @@ func HandleStart(w http.ResponseWriter, r *http.Request) {
 // HandleMove is called for each turn of each game.
 // Valid responses are "up", "down", "left", or "right".
 func HandleMove(w http.ResponseWriter, r *http.Request) {
-	request := datatypes.GameRequest{}
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// define our head
-	head := request.You.Head
-
-	// if there is a potential head to head, go for it if we can win, else avoid
-	move := "null"
-	move = snake.DetectHeadToHead(&head, &request.Board, request.You.Length)
-
-	// else, if we are in hazard and health is <=50, find the closest not-hazard square and move towards it if possible
-	if move == "null" {
-		move = snake.HandleHazard(&head, int(request.You.Health), &request.Board)
-	} // end if move == null
-
-	// else, find closest food and path to it if possible
-	if move == "null" {
-		// TODO: put this in a function in snake.go
-		dist := 90000 // TODO: change this to actual max value of int, lookup golang spec
-		food := request.Board.Food
-		var closestFood datatypes.Coord
-		for i := 0; i < len(food); i++ {
-			if (food[i].X-head.X)*(food[i].X-head.X)+(food[i].Y-head.Y)*(food[i].Y-head.Y) < dist {
-				dist = (food[i].X-head.X)*(food[i].X-head.X) + (food[i].Y-head.Y)*(food[i].Y-head.Y)
-				closestFood = food[i]
-			}
-		}
-
-		// attempt to go in the direction of the closestFood
-		move = snake.MoveInDirection(&head, &closestFood, &request.Board)
-	}
-
-	// if all other cases don't apply, pick a move that results in moving towards the most amount of space
-	if move == "null" {
-		move = snake.HandleDefaultMove(&request.You.Head, &request.Board, 5, 6)
-	}
-
-	// set up response
-	response := datatypes.MoveResponse{
-		Move: move,
-	}
-
-	fmt.Printf("CHOSEN MOVE: %s\n\n", response.Move)
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Fatal(err)
-	}
+	snake.OnMove(w, r)
 }
 
 // HandleEnd is called when a game your Battlesnake was playing has ended.
--- snake/snake.go
+++ snake/snake.go
@@ -1,12 +1,69 @@
 package snake
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/joshtenorio/ninemo-bot/datatypes"
 	"github.com/joshtenorio/ninemo-bot/snake/api"
 	"github.com/joshtenorio/ninemo-bot/snake/floodfill"
 )
+
+func OnMove(w http.ResponseWriter, r *http.Request) {
+	request := datatypes.GameRequest{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// define our head
+	head := request.You.Head
+
+	// if there is a potential head to head, go for it if we can win, else avoid
+	move := "null"
+	move = DetectHeadToHead(&head, &request.Board, request.You.Length)
+
+	// else, if we are in hazard and health is <=50, find the closest not-hazard square and move towards it if possible
+	if move == "null" {
+		move = HandleHazard(&head, int(request.You.Health), &request.Board)
+	} // end if move == null
+
+	// else, find closest food and path to it if possible
+	if move == "null" {
+		// TODO: put this in a function in snake.go
+		dist := 90000 // TODO: change this to actual max value of int, lookup golang spec
+		food := request.Board.Food
+		var closestFood datatypes.Coord
+		for i := 0; i < len(food); i++ {
+			if (food[i].X-head.X)*(food[i].X-head.X)+(food[i].Y-head.Y)*(food[i].Y-head.Y) < dist {
+				dist = (food[i].X-head.X)*(food[i].X-head.X) + (food[i].Y-head.Y)*(food[i].Y-head.Y)
+				closestFood = food[i]
+			}
+		}
+
+		// attempt to go in the direction of the closestFood
+		move = MoveInDirection(&head, &closestFood, &request.Board)
+	}
+
+	// if all other cases don't apply, pick a move that results in moving towards the most amount of space
+	if move == "null" {
+		move = HandleDefaultMove(&request.You.Head, &request.Board, 5, 6)
+	}
+
+	// set up response
+	response := datatypes.MoveResponse{
+		Move: move,
+	}
+
+	fmt.Printf("CHOSEN MOVE: %s\n\n", response.Move)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func IsMoveTrap(board *datatypes.Board, head *datatypes.Coord, move string, searchDist int, minSpaces int) bool {
 	futurePos := api.MoveToCoord(move, head)
